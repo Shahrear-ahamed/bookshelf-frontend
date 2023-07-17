0e84fc03aa1e66b57583fa7b68aa6afc24df7000
Shahrear-ahamed <its.shahrear@gmail.com>
2023-07-18 01:02:17 +0600

    fix all book page

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -4,20 +4,24 @@ import { IBook } from "../types/book";
 
 function AllBooks() {
   const { data: booksData, isLoading } = useGetHomeBooksQuery(undefined);
-  const books = booksData?.data;
+  const books: IBook[] = booksData?.data;
+
   isLoading && <div>Loading...</div>;
 
   return (
     <>
       <div className="my-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {books?.map((book: IBook) => (
-          <div
-            key={book._id}
-            className="group relative p-2 rounded-lg card__shadow">
-            <BookCard book={book} />
-          </div>
-        ))}
+        {books?.length > 0 &&
+          books?.map((book: IBook) => (
+            <div
+              key={book._id}
+              className="group relative p-2 rounded-lg card__shadow">
+              <BookCard book={book} />
+            </div>
+          ))}
       </div>
+
+      {books?.length === 0 && <div className="text-center">No books found</div>}
     </>
   );
 }
